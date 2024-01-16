437a5c99a984dc7c321dd5a7a4a7b24f2dc9a92d
yu ji <leon5617@gmail.com>
2024-01-16 23:44:10 +0900

    新規投稿ページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,25 @@
 class PostsController < ApplicationController
+  
+  # indexメソッド 投稿一覧を並べ替える
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
+  # showメソッド 投稿のidを取得する
   def show
     @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+
+  end
+
+  # createメソッド 投稿(content)をインスタンス生成し取得する。
+  # その後、インスタンを保存し"/posts/index"ページに返ってくる
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+
+    redirect_to("/posts/index")
   end
 end
